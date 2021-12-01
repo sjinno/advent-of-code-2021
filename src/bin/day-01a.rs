dbf6e22be7adb1cb661a33d97674fbf00b5ff2ac
Shohei Jinno <shohei.jinno@gmail.com>
2021-12-01 08:24:49 -0800

    [day 1] Done.

--- src/bin/day-01a.rs
+++ src/bin/day-01a.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use failure::Error;
+use std::io::{self, Read};
+
+fn main() -> Result<(), Error> {
+    let mut buffer = String::new();
+    {
+        let stdin = io::stdin();
+        stdin.lock().read_to_string(&mut buffer)?;
+    }
+
+    let mut lines = buffer.lines();
+    let mut prev = lines.next().unwrap().parse::<usize>()?;
+    let mut counter = 0;
+
+    for line in buffer.lines() {
+        if let Ok(curr) = line.trim().parse::<usize>() {
+            if curr > prev {
+                counter += 1;
+            }
+            prev = curr;
+        }
+    }
+
+    eprintln!("{}", counter);
+
+    Ok(())
 }
--- src/bin/day-01b.rs
+++ src/bin/day-01b.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use failure::Error;
+use std::io::{self, Read};
+
+fn main() -> Result<(), Error> {
+    let mut buffer = String::new();
+    {
+        let stdin = io::stdin();
+        stdin.lock().read_to_string(&mut buffer)?;
+    }
+
+    let data = buffer
+        .trim()
+        .split('\n')
+        .map(|elt| elt.trim().parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut prev = data[0] + data[1] + data[2];
+    let mut counter = 0;
+
+    for i in 1..data.len() - 2 {
+        let curr = data[i] + data[i + 1] + data[i + 2];
+        if curr > prev {
+            counter += 1;
+        }
+        prev = curr;
+    }
+
+    eprintln!("{}", counter);
+
+    Ok(())
 }
