965c6706bfec6df4d7308ecab1dfb7f258426aeb
Shohei Jinno <shohei.jinno@gmail.com>
2021-12-05 20:16:52 -0800

    [day 5] A bit of rafactoring.

--- src/bin/day-05a.rs
+++ src/bin/day-05a.rs
@@ -21,32 +21,28 @@ fn main() -> Result<(), Error> {
         stdin.lock().read_to_string(&mut buf)?;
     }
 
-    let lines = buf
+    let lines: Vec<Line> = buf
         .lines()
         .map(|line| {
-            let split = line
-                .split("->")
-                .map(|p| {
-                    let mut point = p.trim().split(',');
-                    Point {
-                        x: point.next().unwrap().parse::<usize>().unwrap(),
-                        y: point.next().unwrap().parse::<usize>().unwrap(),
-                    }
-                })
-                .collect::<Vec<Point>>();
+            let mut points = line.split("->").map(|p| {
+                let mut point = p.trim().split(',');
+                Point {
+                    x: point.next().unwrap().parse::<usize>().unwrap(),
+                    y: point.next().unwrap().parse::<usize>().unwrap(),
+                }
+            });
 
             Line {
-                p0: split[0].clone(),
-                p1: split[1].clone(),
+                p0: points.next().unwrap(),
+                p1: points.next().unwrap(),
             }
         })
-        .collect::<Vec<Line>>();
+        .collect();
 
-    let mut hm = HashMap::<Point, usize>::new();
+    let mut visited = HashMap::<Point, usize>::new();
 
     for line in lines {
         let (p0, p1) = (line.p0, line.p1);
-
         let coord0 = (p0.x, p0.y);
         let coord1 = (p1.x, p1.y);
 
@@ -54,14 +50,14 @@ fn main() -> Result<(), Error> {
             ((x0, y0), (x1, y1)) if x0 == x1 => {
                 let (s, e) = if y0 < y1 { (y0, y1) } else { (y1, y0) };
                 for i in s..=e {
-                    let counter = hm.entry(Point { x: x0, y: i }).or_insert(0);
+                    let counter = visited.entry(Point { x: x0, y: i }).or_insert(0);
                     *counter += 1;
                 }
             }
             ((x0, y0), (x1, y1)) if y0 == y1 => {
                 let (s, e) = if x0 < x1 { (x0, x1) } else { (x1, x0) };
                 for i in s..=e {
-                    let counter = hm.entry(Point { x: i, y: y0 }).or_insert(0);
+                    let counter = visited.entry(Point { x: i, y: y0 }).or_insert(0);
                     *counter += 1;
                 }
             }
@@ -69,14 +65,9 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    let mut count = 0;
-
-    for (_key, val) in &hm {
-        if val > &1 {
-            count += 1;
-        }
-    }
-
+    let count = visited
+        .into_values()
+        .fold(0, |acc, v| if v > 1 { acc + 1 } else { acc });
     println!("count: {}", count);
 
     Ok(())
